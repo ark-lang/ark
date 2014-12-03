76ebad7cb7a15780482a2f9cca5917f015c3b0a7
Felix Angell <felixangell1024@gmail.com>
2014-12-03 15:34:52 +0000

    file io and reading working

--- src/main.c
+++ src/main.c
@@ -7,26 +7,26 @@ char *readFile(const char *fileName) {
 	FILE *file = fopen(fileName, "r");
 	char *contents;
 
-	if (file) {
-		if (fseek(file, 0, SEEK_END)) {
+	if (file != NULL) {
+		if (fseek(file, 0, SEEK_END) == 0) {
 			long fileSize = ftell(file);
 			if (fileSize == -1) {
 				perror("ftell: could not read filesize");
 				exit(1);
 			}
 			contents = malloc(sizeof(*contents) * (fileSize + 1));
-			if (!contents) {
+			if (contents == NULL) {
 				perror("malloc: failed to allocate memory for file");
 				exit(1);
 			}
 
-			if (fseek(file, 0, SEEK_SET)) {
+			if (fseek(file, 0, SEEK_SET) != 0) {
 				perror("could not reset file index");
 				exit(1);
 			}
 
 			size_t fileLength = fread(contents, sizeof(char), fileSize, file);
-			if (!fileLength) {
+			if (fileLength == 0) {
 				perror("fread: file is empty");
 				exit(1);
 			}
@@ -43,15 +43,9 @@ char *readFile(const char *fileName) {
 	return contents;
 }
 
-int main(int argc, char** argv) {
-	if (argc > 0) {
-		int i;
-		for (i = 0; i < argc; i++) {
-			printf("arg: %s\n", argv[i]);
-		}
-	}
-
-	Lexer *lexer = createLexer("this is a test 123 wow");
+void startCompiling(char *source) {
+	Lexer *lexer = createLexer(source);
+	printf("lexing:\n %s\n", source);
 
 	do {
 		getNextToken(lexer);
@@ -67,6 +61,21 @@ int main(int argc, char** argv) {
 	while (lexer->token.class != END_OF_FILE);
 
 	destroyLexer(lexer);
+}
+
+int main(int argc, char** argv) {
+	if (argc > 1) {
+		char *fileName = argv[1];
+		char *sourceCode = readFile(fileName);
+
+		startCompiling(sourceCode);
+
+		free(sourceCode);
+	}
+	else {
+		printf("error: no input files\n");
+		exit(1);
+	}
 
 	return 0;
 }
