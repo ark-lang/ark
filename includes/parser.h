0d655662d937f96fc3997f50d51cadc192ab9574
Vedant N <vnev@Vedants-Mac-Pro.local>
2014-12-07 02:36:22 +0530

    changes

---
+++ includes/parser.h
@@ -0,0 +1,33 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+// Class
+
+typedef struct tree {
+  char* val;
+  tree* right;
+  tree* left;
+} Tree;
+
+void insert(Tree ** tr, int value) {
+  Tree *temp = NULL;
+
+  if(!(*tr)) {
+    temp = malloc(sizeof(Tree));
+    temp->left = temp->right = NULL;
+    temp->data = val;
+    *tr = temp;
+    return;
+  }
+
+  if(value < (*tr)->data)
+    insert(&(*tree)->left, value);
+  else if(value > (*tree)->data)
+    insert(&(*tree)->right, value);
+  }
+}
+
+
+void delete_from_tree(Tree ** tr);
+
+#ifndef PARSER_H
