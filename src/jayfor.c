534515b3e88158836cac7a5a450217750cdbbbfd
Felix Angell <freefouran@gmail.com>
2014-12-07 15:45:50 +0000

    removed debugging code

--- src/jayfor.c
+++ src/jayfor.c
@@ -2,14 +2,14 @@
 
 Jayfor *jayforCreate(int argc, char** argv) {
 	// not enough args just throw an error
-	// if (argc <= 1) {
-	// 	printf("error: no input files\n");
-	// 	exit(1);
-	// }
+	if (argc <= 1) {
+		printf("error: no input files\n");
+		exit(1);
+	}
 
 	Jayfor *jayfor = malloc(sizeof(*jayfor));
 	jayfor->scanner = scannerCreate();
-	scannerReadFile(jayfor->scanner, "tests/test.j4"); //argv[1]
+	scannerReadFile(jayfor->scanner, argv[1]);
 	jayfor->lexer = lexerCreate(jayfor->scanner->contents);
 
 	return jayfor;
