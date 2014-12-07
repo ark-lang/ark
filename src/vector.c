f52f195e8f0bd95006e39715f05be935e84dff56
Felix Angell <freefouran@gmail.com>
2014-12-07 21:42:32 +0000

    fixed all the include error stuffs

--- src/vector.c
+++ src/vector.c
@@ -2,9 +2,17 @@
 
 Vector *vectorCreate() {
 	Vector *vec = malloc(sizeof(*vec));
+	if (!vec) {
+		perror("malloc: failed to allocate memory for Vector");
+		exit(1);
+	}
 	vec->size = 0;
 	vec->maxSize = 2;
 	vec->items = malloc(sizeof(*vec->items) * vec->maxSize);
+	if (!vec->items) {
+		perror("malloc: failed to allocate memory for vector contents");
+		exit(1);
+	}
 	return vec;
 }
 
@@ -12,6 +20,10 @@ void vectorPushBack(Vector *vec, VectorItem item) {
 	if (vec->size >= vec->maxSize) {
 		vec->maxSize *= 2;
 		vec->items = realloc(vec->items, sizeof(*vec->items) * vec->maxSize);
+		if (!vec->items) {
+			perror("realloc: failed to allocate memory for vector contents");
+			exit(1);
+		}
 	}
 	vec->items[vec->size++] = item;
 }
