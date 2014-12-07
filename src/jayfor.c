8b9e00687a18b4cdc6f8b729f2bd2890fe624048
Felix Angell <felix.angell15@gmail.com>
2014-12-07 15:42:16 +0000

    added vector, token stream created too

--- src/jayfor.c
+++ src/jayfor.c
@@ -1,24 +1,25 @@
 #include "jayfor.h"
 
 Jayfor *jayforCreate(int argc, char** argv) {
-	if (argc <= 1) {
-		printf("error: no input files\n");
-		exit(1);
-	}
+	// not enough args just throw an error
+	// if (argc <= 1) {
+	// 	printf("error: no input files\n");
+	// 	exit(1);
+	// }
 
 	Jayfor *jayfor = malloc(sizeof(*jayfor));
 	jayfor->scanner = scannerCreate();
-	scannerReadFile(jayfor->scanner, argv[1]);
+	scannerReadFile(jayfor->scanner, "tests/test.j4"); //argv[1]
 	jayfor->lexer = lexerCreate(jayfor->scanner->contents);
+
 	return jayfor;
 }
 
 void jayforStart(Jayfor *jayfor) {
 	do {
 		lexerGetNextToken(jayfor->lexer);
-		printf("%s\n", jayfor->lexer->token.content);
 	}
-	while (jayfor->lexer->token.type != END_OF_FILE);
+	while (jayfor->lexer->running);
 }
 
 void jayforDestroy(Jayfor *jayfor) {
