66521b457416d665bb888dc66037b2d739cb75ba
Felix Angell <felix.angell15@gmail.com>
2014-12-07 22:44:36 +0000

    EOF now gets added to token stream

--- src/parser.c
+++ src/parser.c
@@ -7,7 +7,21 @@ Parser *parserCreate(Vector *tokenStream) {
 		exit(1);
 	}
 	parser->tokenStream = tokenStream;
+	parser->tokenIndex = 0;
+	parser->parsing = true;
 	return parser;
+}
+
+void parserStartParsing(Parser *parser) {
+	while (parser->parsing) {
+		Token *tok = vectorGetItem(parser->tokenStream, parser->tokenIndex);
+		if (tok->type == END_OF_FILE) {
+			parser->parsing = false;
+			break;
+		}
+		printf("%s\n", tok->content);
+		parser->tokenIndex += 1;
+	}
 }
 
 void parserDestroy(Parser *parser) {
