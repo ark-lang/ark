6870d878aff21bfc67d8454a2f476cb26ec1ed85
Felix Angell <freefouran@gmail.com>
2014-12-07 14:29:12 +0000

    more comments2

--- includes/scanner.h
+++ includes/scanner.h
@@ -6,14 +6,34 @@
 
 #include "util.h"
 
+/**
+ * Scanner properties,
+ * todo: allow multiple files
+ * 		 to be scanned.
+ */
 typedef struct {
 	string contents;
 } Scanner;
 
+/**
+ * Creates an instance of a scanner
+ * 
+ * @return the scanner
+ */
 Scanner *scannerCreate();
 
+/**
+ * Reads the given file into
+ * `string contents;`
+ * 
+ */
 void scannerReadFile(Scanner *scanner, const string fileName);
 
+/**
+ * Destroys the given Scanner
+ * 
+ * @param scanner scanner to destroy
+ */
 void scannerDestroy(Scanner *scanner);
 
 #endif // SCANNER_H
