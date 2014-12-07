26142c06335a9458d984213de78d192887a86798
Felix Angell <freefouran@gmail.com>
2014-12-07 12:55:27 +0000

    loads of comments and a license

--- src/lexer.c
+++ src/lexer.c
@@ -1,105 +1,102 @@
 #include "lexer.h"
 
-Lexer *createLexer(char *input) {
+Lexer *lexerCreate(char *input) {
 	Lexer *lexer = malloc(sizeof(*lexer));
 	if (!lexer) {
-		perror("malloc: lexer");
+		perror("malloc: failed to allocate memory for lexer");
 	}
 	lexer->input = input;
-	lexer->dot = 0;
-	lexer->inputChar = input[lexer->dot];
-	lexer->token.class = UNKNOWN;
-	lexer->token.repr = "";
+	lexer->pos = 0;
+	lexer->charIndex = input[lexer->pos];
+	lexer->token.type = UNKNOWN;
+	lexer->token.content = "";
 	return lexer;
 }
 
-void nextChar(Lexer *lexer) {
-	lexer->inputChar = lexer->input[++lexer->dot];
+void lexerNextChar(Lexer *lexer) {
+	lexer->charIndex = lexer->input[++lexer->pos];
 }
 
-char *inputToZString(Lexer *lexer, int start, int length) {
+char *lexerFlushBuffer(Lexer *lexer, int start, int length) {
 	char *result;
 	strncpy(result = malloc(length + 1), &lexer->input[start], length);
+	if (!result) { perror("malloc: failed to allocate memory for buffer flush"); }
 	result[length] = '\0';
 	return result;
 }
 
-void skipLayoutAndComment(Lexer *lexer) {
-	while (isLayout(lexer->inputChar)) {
-		nextChar(lexer);
+void lexerSkipLayoutAndComment(Lexer *lexer) {
+	while (isLayout(lexer->charIndex)) {
+		lexerNextChar(lexer);
 	}
-	while (isCommentOpener(lexer->inputChar)) {
-		nextChar(lexer);
-		while (!isCommentCloser(lexer->inputChar)) {
-			if (isEndOfInput(lexer->inputChar)) return;
-			nextChar(lexer);
+	while (isCommentOpener(lexer->charIndex)) {
+		lexerNextChar(lexer);
+		while (!isCommentCloser(lexer->charIndex)) {
+			if (isEndOfInput(lexer->charIndex)) return;
+			lexerNextChar(lexer);
 		}
-		nextChar(lexer);
-		while (isLayout(lexer->inputChar)) {
-			nextChar(lexer);
-		}
-	}
-}
-
-void recognizeIdentifier(Lexer *lexer) {
-	lexer->token.class = IDENTIFIER;
-	nextChar(lexer);
-
-	while (isLetterOrDigit(lexer->inputChar)) {
-		nextChar(lexer);
-	}
-	while (isUnderscore(lexer->inputChar) && isLetterOrDigit(lexer->input[lexer->dot + 1])) {
-		nextChar(lexer);
-		while (isLetterOrDigit(lexer->inputChar)) {
-			nextChar(lexer);
+		lexerNextChar(lexer);
+		while (isLayout(lexer->charIndex)) {
+			lexerNextChar(lexer);
 		}
 	}
 }
 
-void recognizeInteger(Lexer *lexer) {
-	lexer->token.class = INTEGER;
-	nextChar(lexer);
-	while (isDigit(lexer->inputChar)) {
-		nextChar(lexer);
+void lexerRecognizeIdentifier(Lexer *lexer) {
+	lexer->token.type = IDENTIFIER;
+	lexerNextChar(lexer);
+
+	while (isLetterOrDigit(lexer->charIndex)) {
+		lexerNextChar(lexer);
+	}
+	while (isUnderscore(lexer->charIndex) && isLetterOrDigit(lexer->input[lexer->pos + 1])) {
+		lexerNextChar(lexer);
+		while (isLetterOrDigit(lexer->charIndex)) {
+			lexerNextChar(lexer);
+		}
 	}
 }
 
-void getNextToken(Lexer *lexer) {
-	int startDot;
+void lexerRecognizeInteger(Lexer *lexer) {
+	lexer->token.type = INTEGER;
+	lexerNextChar(lexer);
+	while (isDigit(lexer->charIndex)) {
+		lexerNextChar(lexer);
+	}
+}
 
-	skipLayoutAndComment(lexer);
+void lexerGetNextToken(Lexer *lexer) {
+	int startPos;
 
-	startDot = lexer->dot;
-	if (isEndOfInput(lexer->inputChar)) {
-		lexer->token.class = END_OF_FILE;
-		lexer->token.repr = "<END_OF_FILE>";
+	lexerSkipLayoutAndComment(lexer);
+
+	startPos = lexer->pos;
+	if (isEndOfInput(lexer->charIndex)) {
+		lexer->token.type = END_OF_FILE;
+		lexer->token.content = "<END_OF_FILE>";
 		return;
 	}
-	if (isLetter(lexer->inputChar)) {
-		recognizeIdentifier(lexer);
+	if (isLetter(lexer->charIndex)) {
+		lexerRecognizeIdentifier(lexer);
 	}
-	else if (isDigit(lexer->inputChar)) {
-		recognizeInteger(lexer);
+	else if (isDigit(lexer->charIndex)) {
+		lexerRecognizeInteger(lexer);
 	}
-	else if (isOperator(lexer->inputChar)) {
-		lexer->token.class = OPERATOR;
-		nextChar(lexer);
+	else if (isOperator(lexer->charIndex)) {
+		lexer->token.type = OPERATOR;
+		lexerNextChar(lexer);
 	}
-	else if (isSeparator(lexer->inputChar)) {
-		lexer->token.class = SEPARATOR;
-		nextChar(lexer);
+	else if (isSeparator(lexer->charIndex)) {
+		lexer->token.type = SEPARATOR;
+		lexerNextChar(lexer);
 	}
 	else {
-		lexer->token.class = ERRORNEOUS;
-		nextChar(lexer);
+		lexer->token.type = ERRORNEOUS;
+		lexerNextChar(lexer);
 	}
-	lexer->token.repr = inputToZString(lexer, startDot, lexer->dot - startDot);
+	lexer->token.content = lexerFlushBuffer(lexer, startPos, lexer->pos - startPos);
 }
 
-bool isDataType(char *type) {
-	return (!strcmp(type, "int") || !strcmp(type, "bool"));
-}
-
-void destroyLexer(Lexer *lexer) {
+void lexerDestroy(Lexer *lexer) {
 	free(lexer);
 }
