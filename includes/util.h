1c7ae15ef4bb24ec8f44d59181a3700af46a3b68
Felix Angell <felix.angell15@gmail.com>
2014-12-07 13:06:03 +0000

    peeking function

--- includes/util.h
+++ includes/util.h
@@ -1,8 +1,12 @@
 #ifndef BOOL_H
 #define BOOL_H
 
+/** quick boolean implementation */
 typedef enum {
 	false, true
 } bool;
+
+/** typedef for easy strings */
+typedef char* string;
 
 #endif // BOOL_H
