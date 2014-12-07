09fa82ebe6d75cfc5698167d50620d71b823c028
Felix Angell <felix.angell15@gmail.com>
2014-12-07 17:17:22 +0000

    push

--- includes/parser.h
+++ includes/parser.h
@@ -1,14 +1,26 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+/**
+ * Node for a Number
+ */
 typedef struct {
 	double value;
 } NumberExpr;
 
+/**
+ * Node for a variable call
+ * e.g int a = b + c;
+ */
 typedef struct {
 	char* name;
 } VariableExpr;
 
+/**
+ * Node for a binary expression,
+ * e.g
+ * a + 5
+ */
 typedef struct {
 	char operand;
 	Expr *lhs;
@@ -19,8 +31,19 @@ typedef struct {
 	Vector *tokenStream;
 } Parser;
 
+/**
+ * Create a new Parser instance
+ * 
+ * @param tokenStream the token stream to parse
+ * @return instance of Parser
+ */
 Parser *parserCreate(Vector *tokenStream);
 
+/**
+ * Destroy the given Parser
+ * 
+ * @param parser the parser to destroy
+ */
 void parserDestroy(Parser *parser);
 
 #endif // PARSER_H
