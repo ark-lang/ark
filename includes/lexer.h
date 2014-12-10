d7f67756d6bc37a538d17c0626b406117b61cb4c
Vedant <captainchloride@gmail.com>
2014-12-10 23:34:14 +0530

    Fix minor issue on line 254

--- includes/lexer.h
+++ includes/lexer.h
@@ -251,6 +251,6 @@ static inline bool isSeparator(char ch) 		{ return (strchr(" ;,.`@(){}[] ", ch) 
  * @return if the character is a special character like the British symbol or alike 
  * @param ch character to check
  */
-static inline bool isSpecialChar(char ch)		{ return (int) ch >= WEIRD_CHARACTER_THRESHOLD; }
+static inline bool isSpecialChar(char ch)		{ return (int) ch >= WEIRD_CHARACTER_ASCII_THRESHOLD; }
 	
 #endif // LEXER_H
