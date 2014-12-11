452126bf039f4aead17872daba64f195e06bbbae
Felix Angell <felixangell1024@gmail.com>
2014-12-11 19:36:18 +0000

    added time taken to execute program

--- src/main.c
+++ src/main.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <time.h>
 
 #include "jayfor.h"
 
 int main(int argc, char** argv) {
-	// pass arguments through so the jayfor backend
-	// can deal with it
+
+	// for calculating time taken
+	clock_t timer = clock();
+
+	// jayfor stuff
 	Jayfor *jayfor = jayforCreate(argc, argv);
 	jayforStart(jayfor);
 	jayforDestroy(jayfor);
+
+	// calculate time
+	timer = clock() - timer;
+	double timeTaken = ((double) timer) / CLOCKS_PER_SEC;
+	printf("Finished in %f/s\n", timeTaken);
 
 	return 0;
 }
