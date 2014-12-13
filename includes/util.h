9dac475378af1b5b38496993dda3af0ce9995eff
Felix Angell <felix.angell15@gmail.com>
2014-12-13 13:11:02 +0000

    added some colours for errors because why not?

--- includes/util.h
+++ includes/util.h
@@ -1,6 +1,26 @@
 #ifndef BOOL_H
 #define BOOL_H
 
+#ifdef _WIN32
+	#define KNRM  ""
+	#define KRED  ""
+	#define KGRN  ""
+	#define KYEL  ""
+	#define KBLU  ""
+	#define KMAG  ""
+	#define KCYN  ""
+	#define KWHT  ""
+#elif __linux
+	#define KNRM  "\x1B[0m"
+	#define KRED  "\x1B[31m"
+	#define KGRN  "\x1B[32m"
+	#define KYEL  "\x1B[33m"
+	#define KBLU  "\x1B[34m"
+	#define KMAG  "\x1B[35m"
+	#define KCYN  "\x1B[36m"
+	#define KWHT  "\x1B[37m"
+#endif
+
 /** quick boolean implementation */
 typedef enum {
 	false, true
