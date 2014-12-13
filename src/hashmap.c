74a6b79ea1481e9efb1631beb413775c9d58bb2c
Felix Angell <felix.angell15@gmail.com>
2014-12-13 22:30:47 +0000

    added a hashmap

---
+++ src/hashmap.c
@@ -0,0 +1,81 @@
+#include "hashmap.h"
+
+Hashmap *hashmapCreate() {
+	Hashmap *map = malloc(sizeof(*map));
+	map->size = 128;
+	map->fields = malloc(sizeof(Field) * map->size);
+	int i;
+	for (i = 0; i < map->size; i++) {
+		Field *field = map->fields + i;
+		field->size = 0;
+		field->entries = NULL;
+	}
+	return map;
+}
+
+void *hashmapGet(Hashmap *map, char *key) {
+	int hash = hashmapHash(key, map->size - 1);
+	Field *field = map->fields + hash;
+	Entry *entry;
+
+	int i;
+	for (i = 0; i < field->size; i++) {
+		entry = field->entries + i;
+		if (!strcmp(entry->key, key)) {
+			return memcpy(malloc(entry->length), entry->value, entry->length);
+		}
+	}
+	return NULL;
+}
+
+void hashmapSet(Hashmap *map, char *key, void *value, size_t length) {
+	int hash = hashmapHash(key, map->size - 1);
+	Field *field = map->fields + hash;
+	Entry *entry;
+
+	int i;
+	for (i = 0; i < field->size; i++) {
+		entry = field->entries + i;
+		if (!strcmp(entry->key, key)) {
+			free(entry->value);
+			goto setValue;
+		}
+	}
+	if (value == NULL) return;
+	field->size++;
+	field->entries = realloc(field->entries, field->size * sizeof(Entry));
+	entry = field->entries + field->size - 1;
+	entry->key = strdup(key);
+
+	setValue:
+	if (!value) {
+		entry->value = memcpy(malloc(length), value, length);
+		entry->length = length;
+	}
+	else {
+		free(entry->key);
+		field->size--;
+		if (entry != (field->entries + field->size)) {
+			memcpy(entry, (field->entries + field->size), sizeof(Entry));
+		}
+		field->entries = realloc(field->entries, field->size * sizeof(Entry));
+	}
+}
+
+void hashmapDestroy(Hashmap *map) {
+	int i;
+	for (i = 0; i < map->size; i++) {
+		Field *field = map->fields + i;
+		if (!field->entries) {
+			int j;
+			for (j = 0; j < field->size; j++) {
+				Entry *entry = field->entries + j;
+				free(entry->key);
+				free(entry->value);
+			}
+			free(field->entries);
+		}
+	}
+	free(map->fields);
+	free(map);
+}
