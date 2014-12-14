c2507809108b6b9ab1c5de5cfa36785c307fac48
Felix Angell <felix.angell15@gmail.com>
2014-12-14 17:53:08 +0000

    removed LLVM

--- includes/codegen.h
+++ includes/codegen.h
@@ -1,13 +1,11 @@
 #ifndef CODEGEN_H
 #define CODEGEN_H
 
-#include <llvm-c/Core.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct {
-	LLVMModuleRef module;
-	LLVMBuilderRef builder;
+	
 } Backend;
 
 Backend *backendCreate();
