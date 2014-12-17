4411c31ccaba11a242a10b41a5b09bb1b63569f4
Felix <freefouran@gmail.com>
2014-12-17 22:50:35 +0000

    fixed stupid hashmap thing

--- includes/hashmap.h
+++ includes/hashmap.h
@@ -2,75 +2,30 @@
 #define HASHMAP_H
 
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
 
-#include "util.h"
-
-/**
- * An entry in our hashmap
- */
 typedef struct {
 	char *key;
-	char *value;
-	size_t length;
-} Entry;
+	void *val;
+	size_t len;
+} HashmapEntry;
 
-/**
- * A field in our hashmap 
- */
 typedef struct {
 	int size;
-	Entry *entries;
-} Field;
+	HashmapEntry *entries;
+} HashmapField;
 
-/**
- * Hashmap representation
- */
 typedef struct {
 	int size;
-	Field *fields;
+	HashmapField *fields;
 } Hashmap;
+ 
+Hashmap *createHashmap(int);
 
-/**
- * Create a new hashmap
- */
-Hashmap *createHashmap();
-
-/**
- * Set the value in the hashmap
- */
-void setValueAtKey(Hashmap *map, char *key, void *value, size_t length);
-
-/**
- * Retrieves value at the given key from the hashmap
- */
-void *getValueAtKey(Hashmap *map, char *key);
-
-/**
- * Destroy the given hashmap
- */
 void destroyHashmap(Hashmap *map);
 
-/**
- * Simple hashing algorithm
- */
-static inline unsigned long int hashmapFNV1A(void *data, unsigned long int length) {
-	unsigned char *p = (unsigned char *) data;
-	unsigned long int h = 2166136261UL;
-	unsigned long int i;
-	for (i = 0; i < length; i++) {
-		h = (h ^ p[i]) * 16777619;
-	}
-	return h;
-}
+void *getValueAtKey(Hashmap *map, char* str);
 
-/**
- * Hash the given string
- */
-static inline int hashmapHash(char *str, int maxHash) {
-	unsigned long int fnv1aHash = hashmapFNV1A(str, strlen(str));
-	return (int) (fnv1aHash % (maxHash + 1));
-}
+void setValueAtKey(Hashmap  *map, char* str, void* data, size_t size);
 
 #endif // HASHMAP_H
