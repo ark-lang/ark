249f4c068eaf935392ece12f84ea4d5c6582db54
Felix <freefouran@gmail.com>
2014-12-17 19:29:21 +0000

    expressions are evaluated
    for now it just evaluates it then generates code e.g int x = (5 + 5)
    would evaluate as 10 then store the 10. Not sure if I should evaluate it
    as direct bytecode, e.g push 5 push 5 add or something

--- src/compiler.c
+++ src/compiler.c
@@ -6,6 +6,10 @@ static const char* DATA_TYPES[] = {
 	"void", "char", "tup"
 };
 
+static const char* INSTRUCTION_SET[] = {
+	"ADD", "SUB", "MUL", "RET", "ICONST", "LOAD", "GLOAD", "STORE", "GSTORE", "POP", "HALT"
+};
+
 Compiler *createCompiler() {
 	Compiler *self = malloc(sizeof(*self));
 	self->ast = NULL;
@@ -29,17 +33,34 @@ void consumeNode(Compiler *self) {
 	self->currentNode += 1;
 }
 
+int evaluateExpressionNode(Compiler *self, ExpressionNode *expr) {
+	int result = 0;
+	if (expr->value != NULL) {
+		result = atoi(expr->value->content);
+	}
+	else {
+		int left = evaluateExpressionNode(self, expr->lhand);
+		int right = evaluateExpressionNode(self, expr->rhand);
+
+		switch (expr->operand) {
+			case '+': result = left + right; break;
+			case '-': result = left - right; break;
+			case '*': result = left * right; break;
+			case '/': result = left / right; break;
+			case '%': result = left % right; break;
+			case '>': result = left > right; break;
+			case '<': result = left < right; break;
+			case '^': result = left ^ right; break;
+		}
+	}
+	return result;
+}
+
 void generateVariableDeclarationCode(Compiler *self, VariableDeclareNode *vdn) {
 	DataType type = vdn->vdn->type;
 	Token *name = vdn->vdn->name;
 	ExpressionNode *expr = vdn->expr;
-
-	if (expr->type != EXPR_NUMBER) {
-		printf("unsupported expression.\n");
-		exit(1);
-	}
-
-	int result = atoi(expr->value->content);
+	int result = evaluateExpressionNode(self, expr);
 
 	printf("%s %s = %d\n", DATA_TYPES[type], name->content, result);
 
@@ -71,8 +92,8 @@ void startCompiler(Compiler *self, Vector *ast) {
 	appendInstruction(self, HALT);
 
 	int i;
-	for (i = 0; i < self->currentInstruction; i++) {
-		printf("%d\n", self->bytecode[i]);
+	for (i = 0; i < self->currentInstruction; i += 2) {
+		printf("%s %d\n", INSTRUCTION_SET[self->bytecode[i]], self->bytecode[i + 1]);
 	}
 }
 
