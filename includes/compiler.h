87ef7941d371b571210c759121108906566c0473
Felix <freefouran@gmail.com>
2014-12-17 19:04:35 +0000

    code generation for simple variable declaration

--- includes/compiler.h
+++ includes/compiler.h
@@ -4,15 +4,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "parser.h"
 #include "vector.h"
 #include "j4vm.h"
 
 typedef struct {
 	Vector *ast;
 	JayforVM *vm;
+	int *bytecode;
+	int initialBytecodeSize;
+	int maxBytecodeSize;
+	int currentNode;
+	int currentInstruction;
+	int globalCount;
 } Compiler;
 
 Compiler *createCompiler();
+
+void appendInstruction(Compiler *self, int instr);
+
+void consumeNode(Compiler *self);
+
+void generateVariableDeclarationCode(Compiler *compiler, VariableDeclareNode *vdn);
 
 void startCompiler(Compiler *compiler, Vector *ast);
 
