5b28f7e937c7d1660a20b54aa0dd8ab547461cbd
Felix <freefouran@gmail.com>
2014-12-24 05:19:14 +0000

    docs

---
+++ docs/html/search/all_d.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['name',['name',['../structvariable__define__ast__node.html#a0be25b04f38aae29e2dd7c1b72d4797c',1,'variable_define_ast_node::name()'],['../structfunction__argument__ast__node.html#a0be25b04f38aae29e2dd7c1b72d4797c',1,'function_argument_ast_node::name()'],['../structfunction__prototype__ast__node.html#a0be25b04f38aae29e2dd7c1b72d4797c',1,'function_prototype_ast_node::name()'],['../structvariable__reassignment__node.html#a0be25b04f38aae29e2dd7c1b72d4797c',1,'variable_reassignment_node::name()']]],
+  ['next_5fargument',['next_argument',['../structargument.html#a5a2510e0f7136e387bb61ede9e50b393',1,'argument']]],
+  ['number',['number',['../structobject.html#a7106e2abc437ad981830d14176d15f09',1,'object::number()'],['../lexer_8h.html#afe5ef662303b6b710ea6ee1a944bad0da12a90dfe20486bbe3e075afcd19ef2d0',1,'NUMBER():&#160;lexer.h']]],
+  ['number_5fvalue',['number_value',['../j4vm_8h.html#ab38284f4a6eff2db4c134ef3dcac2076',1,'number_value(object *obj):&#160;j4vm.c'],['../j4vm_8c.html#ab38284f4a6eff2db4c134ef3dcac2076',1,'number_value(object *obj):&#160;j4vm.c']]],
+  ['numofreturnvalues',['numOfReturnValues',['../structfunction__return__ast__node.html#a53a6c9388097639e5b67cd4b368f98d1',1,'function_return_ast_node::numOfReturnValues()'],['../structfunction__ast__node.html#a53a6c9388097639e5b67cd4b368f98d1',1,'function_ast_node::numOfReturnValues()']]]
+];
---
+++ docs/html/search/variables_8.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['j4vm',['j4vm',['../structjayfor.html#ac3106705d25b5b4851814aef6c7eafeb',1,'jayfor']]]
+];
