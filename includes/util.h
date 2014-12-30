3612ec3e6c83e0d2fce181f49b315d0e1cb9c9d6
Felix <freefouran@gmail.com>
2014-12-30 15:33:24 +0000

    fixed argument issues, closes #26

--- includes/util.h
+++ includes/util.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /** the current version of jayfor */
 #define JAYFOR_VERSION "0.0.0"
@@ -50,12 +51,29 @@ extern char* OUTPUT_EXECUTABLE_NAME;
 /**
  * Emitts a debug message to the console if we are in DEBUG MODE
  * @param msg           the message to print
- * @param exit_on_error if we should exit on an error
+ * @param ...			extra arguments
  */
 extern void debug_message(const char *fmt, ...); 
 
+/**
+ * Emitts an error message to the console, will also exit
+ * @param msg           the message to print
+ * @param ... 			extra arguments
+ */
 extern void error_message(const char *fmt, ...);
 
+/**
+ * Emitts a primary message to the console
+ * @param msg           the message to print
+ * @param ... 			extra arguments
+ */
 extern void primary_message(const char *fmt, ...);
+
+/**
+ * Gets the extension of the given file
+ * @param  filename the filename to get the extension
+ * @return          the extension of the file given
+ */
+extern const char *get_filename_ext(const char *filename);
 
 #endif // BOOL_H
