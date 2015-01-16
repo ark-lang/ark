a73a2efd31a9791183c87e22674d7cc8e91676d8
Felix Angell <freefouran@gmail.com>
2015-01-16 21:25:24 +0000

    will do pproc later

--- src/preprocessor.c
+++ src/preprocessor.c
@@ -8,11 +8,16 @@
 preprocessor *create_preprocessor(vector *token_stream) {
 	preprocessor *self = safe_malloc(sizeof(*self));
 	self->token_stream = token_stream;
+	self->token_index = 0;
 	return self;
 }
 
+void parse_macro(vector *macro) {
+	// todo
+}
+
 void start_preprocessing(preprocessor *self) {
-	
+	// todo
 }
 
 void destroy_preprocessor(preprocessor *self) {
