a29ebb2f8a65b9d3abd87f1f77d287c3d9dd875e
Felix Angell <freefouran@gmail.com>
2015-01-18 18:11:57 +0000

    updated readme

--- src/preprocessor.c
+++ src/preprocessor.c
@@ -3,6 +3,16 @@
 /**
  * for now this will be a really badly implemented
  * preprocessor for simplicity-sake
+ *
+ * I'm hoping to implement something similar to Rusts macro
+ * system. Maybe macros will be prefix with an exclamation,
+ * i.e
+ *
+ * !use file
+ * !define whatever "whatever"
+ *
+ * However, we would actually work with the AST as opposed to
+ * c's preproccessor just does a replace on all instances.
  */
 
 preprocessor *create_preprocessor(vector *token_stream) {
