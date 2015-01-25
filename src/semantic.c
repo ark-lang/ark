74d2a503087a77ddba19212a8e93c91033526bb9
Felix Angell <freefouran@gmail.com>
2015-01-25 14:23:29 +0000

    working on semantic analysis, wooo agile development

--- src/semantic.c
+++ src/semantic.c
@@ -3,7 +3,19 @@
 semantic *create_semantic_analyser(vector *tree) {
     semantic *self = safe_malloc(sizeof(*self));
     self->tree = tree;
+    self->current_node = 0;
     return self;
+}
+
+void eat_tasty_node(semantic *self) {
+    self->current_node += 1;
+}
+
+void start_analysis(semantic *self) {
+    while (self->current_node < self->tree->size) {
+        printf("yum\n");
+        eat_tasty_node(self);
+    }
 }
 
 void destroy_semantic_analyser(semantic *self) {
