bd5951cd291738093425bd1c717905c1714bcd9d
Felix Angell <freefouran@gmail.com>
2015-01-25 14:23:29 +0000

    working on semantic analysis, wooo agile development

--- includes/semantic.h
+++ includes/semantic.h
@@ -6,9 +6,14 @@
 
 typedef struct {
     vector *tree;
+    int current_node;
 } semantic;
 
 semantic *create_semantic_analyser(vector *tree);
+
+void eat_tasty_node(semantic *self);
+
+void start_analysis(semantic *self);
 
 void destroy_semantic_analyser(semantic *self);
 
