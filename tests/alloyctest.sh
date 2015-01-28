9d5e4b1e5317dc131ba06067e7f57b90472b9595
Felix Angell <freefouran@gmail.com>
2015-01-28 17:59:37 +0000

    methods now parsed, also fixed errors in the unit testing

--- tests/alloyctest.sh
+++ tests/alloyctest.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-TOTAL=$(find parser-tests/*.alloy -type f | wc -l)
+TOTAL=$(find parser-tests/*.aly -type f | wc -l)
 OUTPUT="$(ls)"
 NUM=0
 
-for filename in parser-tests/*.alloy; do
+for filename in parser-tests/*.aly; do
 	OUTPUT="$(alloyc $filename)"
 	((NUM++))
-	if [[ $OUTPUT != "Finished"* ]]; then
+	if [[ $OUTPUT != *"Finished"* ]]; then
 		echo "Failed to parse '$filename', ($NUM/$TOTAL)"
 		echo "$OUTPUT"
 		exit 1
