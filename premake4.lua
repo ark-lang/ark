44993f283435b151c1641a405f5a14ada54493d7
Haneef Mubarak <haneef503@gmail.com>
2015-02-02 21:48:59 -0800

    use premake as build system per #86

---
+++ premake4.lua
@@ -0,0 +1,27 @@
+solution "alloy"
+	configurations { "Debug", "Release" }
+
+	-- get data from shell
+	LLVM_OPTIONS	= "--system-libs --libs "
+	LLVM_CONFIG	= "core analysis executionengine jit interpreter native"
+	LLVM_CFLAGS	= "`" .. "llvm-config --cflags " .. LLVM_OPTIONS .. LLVM_CONFIG .. "`"
+	LLVM_LFLAGS	= "`" .. "llvm-config --ldflags " .. LLVM_OPTIONS .. LLVM_CONFIG .. "`"
+	
+	-- common settings
+	defines { "_GNU_SOURCE" }
+	buildoptions { LLVM_CFLAGS, "-pthread", "-xc" }
+	includedirs { "includes" }
+	links { "dl", "ncurses", "z" }
+	linkoptions { LLVM_LFLAGS, "-pthread", "-Wl,--as-needed -ltinfo" }
+
+	configuration "Debug"
+		flags { "Symbols", "ExtraWarnings", "FatalWarnings" }
+
+	configuration "Release"
+		buildoptions { "-march=native", "-O2" }
+
+	-- compiler
+	project "alloyc"
+		kind "ConsoleApp"
+		language "C++"			-- because LLVM is a bitchling
+		files { "src/*.c", "src/*.h" }
