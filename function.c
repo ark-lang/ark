acf639ca2bf4e247c0decafbf8dabf3d4a873bc7
Felix Angell <mail@felixangell.com>
2015-03-18 21:23:46 +0000

    code generation for if/else, return statements, functions, variable declarations, expressions

--- function.c
+++ function.c
@@ -1,6 +1,18 @@
 #include "function.h"
 #include "stdio.h"
 int add(int a, int b) {
+int x = 5;
+if (x==5) {
+printf("x is five!\n");
 }
-void main() {
+else {
+printf("whatever\n");
+}
+return a+b;
+}
+int main() {
+add(5, 5);
+int x = 5;
+printf("%d\n", x);
+return 0;
 }
