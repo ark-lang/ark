c00cc278b85c8234421f16667d325620b50b6ff4
Felix Angell <mail@felixangell.com>
2015-03-20 22:17:36 +0000

    fixed weird error with function redirects 'leaking'

---
+++ function.h
@@ -0,0 +1,13 @@
+#ifndef __FUNCTION_H
+#define __FUNCTION_H
+
+typedef struct {
+int x;
+int y;
+} Cat;
+int whatever(int x);
+int factorial(float a);
+int test(int *d);
+int main();
+
+#endif // __FUNCTION_H
