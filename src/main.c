b1cdfd6f606a1f8f7f2bdbba20a1f4747bfb824f
Felix Angell <mail@felixangell.com>
2015-03-20 23:03:02 +0000

    fixed #137, also time taken now shown even on error

--- src/main.c
+++ src/main.c
@@ -10,17 +10,17 @@ int main(int argc, char** argv) {
 	AlloyCompiler *alloyc = createAlloyCompiler(argc, argv);
 	if (alloyc) {
 		startAlloyCompiler(alloyc);
-
-		// finished timer
-		timer = clock() - timer;	// calculate time taken
-
-		// calculate seconds and milliseconds taken
-		double secondsTaken = ((double) timer) / CLOCKS_PER_SEC;
-		double msTaken = secondsTaken * 1000;
-
-		primaryMessage("Finished in %.6f/s (%.0f/ms)", secondsTaken, msTaken);
 	}
 	destroyAlloyCompiler(alloyc);
+
+	// finished timer
+	timer = clock() - timer;	// calculate time taken
+
+	// calculate seconds and milliseconds taken
+	double secondsTaken = ((double) timer) / CLOCKS_PER_SEC;
+	double msTaken = secondsTaken * 1000;
+
+	primaryMessage("Finished in %.6f/s (%.0f/ms)", secondsTaken, msTaken);
 
 	return 0;
 }
