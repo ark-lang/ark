2ed8b7012b7826aa328549f0530452100d4712be
Felix Angell <mail@felixangell.com>
2015-03-25 22:17:03 +0000

    moar structs pls

--- src/ast.c
+++ src/ast.c
@@ -1,1 +1,105 @@
 #include "ast.h"
+
+IdentifierList *createIdentifierList() {
+	IdentifierList *list = malloc(sizeof(*list));
+	list->values = createVector();
+	return list;
+}
+
+void destroyIdentifierList(IdentifierList *list) {
+	destroyVector(list->values);
+	free(list);
+}
+
+Expression *createExpression() {
+	Expression *expr = malloc(sizeof(*expr));
+
+	return expr;
+}
+
+void destroyExpression(Expression *expr) {
+	free(expr);
+}
+
+TypeName *createTypeName() {
+	TypeName *typeName = malloc(sizeof(*typeName));
+
+	return typeName;
+}
+
+void destroyTypeName(TypeName *typeName) {
+	free(typeName);
+}
+
+ArrayType *createArrayType() {
+	ArrayType *arrayType = malloc(sizeof(*arrayType));
+
+	return arrayType;
+}
+
+void destroyArrayType(ArrayType *arrayType) {
+	destroyExpression(arrayType->length);
+	free(arrayType);
+}
+
+PointerType *createPointerType() {
+	PointerType *pointerType = malloc(sizeof(*pointerType));
+
+	return pointerType;
+}
+
+void destroyPointerType(PointerType *pointerType) {
+	free(pointerType);
+}
+
+FieldDecl *createFieldDecl() {
+	FieldDecl *fieldDecl = malloc(sizeof(*fieldDecl));
+
+	return fieldDecl;
+}
+
+void destroyFieldDecl(FieldDecl *fieldDecl) {
+	free(fieldDecl);
+}
+
+FieldDeclList *createFieldDeclList() {
+	FieldDeclList *fieldDeclList = malloc(sizeof(*fieldDeclList));
+
+	return fieldDeclList;
+}
+
+void destroyFieldDeclList(FieldDeclList *fieldDeclList) {
+	free(fieldDeclList);
+}
+
+StructType *createStructType() {
+	StructType *structType = malloc(sizeof(*structType));
+
+	return structType;
+}
+
+void destroyStructType(StructType *structType) {
+	free(structType);
+}
+
+FunctionType *createFunctionType() {
+	FunctionType *funcType = malloc(sizeof(*funcType));
+
+	return funcType;
+}
+
+void destroyFunctionType(FunctionType *funcType) {
+	free(funcType);
+}
+
+Type *createType() {
+	Type *type = malloc(sizeof(*type));
+	return type;
+}
+
+void destroyType(Type *type) {
+	destroyArrayType(type->arrayType);
+	destroyPointerType(type->pointerType);
+	destroyStructType(type->structType);
+	destroyTypeName(type->typeName);
+}
--- src/parser.c
+++ src/parser.c
@@ -9,14 +9,50 @@ Parser *createParser() {
 	return parser;
 }
 
+IdentifierList *parseIdentifierList(Parser *parser) {
+	IdentifierList *idenList = createIdentifierList();
+
+	if (checkTokenType(parser, IDENTIFIER, 0)) {
+		pushBackItem(idenList->values, consumeToken(parser)->content);
+
+		while (true) {
+			if (checkTokenTypeAndContent(parser, SEPARATOR, ",", 0)) {
+				consumeToken(parser);
+			}
+			else {
+				errorMessage("expected a comma in identifier list");
+				return NULL;
+			}
+
+			if (checkTokenType(parser, IDENTIFIER, 0)) {
+				pushBackItem(idenList->values, consumeToken(parser)->content);
+				if (!checkTokenTypeAndContent(parser, SEPARATOR, ",", 0)) {
+					break;
+				}
+			}
+		}
+
+		return idenList;
+	}
+
+	return NULL;
+}
+
 Token *consumeToken(Parser *parser) {
 	return getVectorItem(parser->tokenStream, parser->tokenIndex++);
+}
+
+bool checkTokenType(Parser *parser, int type, int ahead) {
+	return peekAtTokenStream(parser, 0)->type == type;
+}
+
+bool checkTokenTypeAndContent(Parser *parser, int type, char *content, int ahead) {
+	return peekAtTokenStream(parser, 0)->type == type && !strcmp(peekAtTokenStream(parser, 0)->content, content);
 }
 
 Token *peekAtTokenStream(Parser *parser, int ahead) {
 	return getVectorItem(parser->tokenStream, parser->tokenIndex + ahead);
 }
-
 
 void startParsingSourceFiles(Parser *parser, Vector *sourceFiles) {
 	int i;
@@ -34,7 +70,14 @@ void startParsingSourceFiles(Parser *parser, Vector *sourceFiles) {
 }
 
 void parseTokenStream(Parser *parser) {
+	while (parser->parsing) {
+		Token *tok = getVectorItem(parser->tokenStream, parser->tokenIndex);
 
+		switch (tok->type) {
+			case IDENTIFIER: break;
+			case END_OF_FILE: parser->parsing = false; break;
+		}
+	}
 }
 
 void destroyParser(Parser *parser) {
