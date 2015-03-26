26f5c768d81004c9709d3abe3cab776feb30bb03
Felix Angell <mail@felixangell.com>
2015-03-26 00:38:30 +0000

    setting up parser, currently everthing but enumerators accounted for

--- includes/ast.h
+++ includes/ast.h
@@ -6,6 +6,8 @@
 
 typedef struct Type s_Type;
 typedef struct Expression s_Expression;
+typedef struct {} ContinueStat;
+typedef struct {} BreakStat;
 
 typedef struct {
 	Vector *values;
@@ -87,17 +89,142 @@ typedef struct {
 
 typedef struct {
 	FieldDeclList *fields;
-} StructType;
+} StructDecl;
+
+typedef enum {
+	MULTI_STATEMENT_BLOCK,
+	SINGLE_STATEMENT_BLOCK
+} BlockType;
 
 typedef struct {
+	StatementList *stmtList;
+	BlockType type;
+} Block;
 
-} FunctionType;
+typedef struct {
+	s_Type *type;
+	IdentifierList *identList;
+} ParameterSection;
+
+typedef struct {
+	Vector *list;
+} ParameterList;
+
+typedef struct {
+	Vector *paramList;
+} Parameters;
+
+typedef struct {
+	s_Type *type;
+	char *name;
+} Receiver;
+
+typedef struct {
+	Receiver *receiver;
+	char *name;
+	Parameters *parameters;
+} FunctionSignature;
+
+typedef struct {
+	FunctionSignature *signature;
+	Block *body;
+} FunctionDecl;
+
+typedef struct {
+	bool mutable;
+	s_Type *type;
+	char *name;
+	s_Expression *expr;
+} VariableDecl;
+
+typedef struct {
+	FunctionDecl *funcDecl;
+	StructDecl *structDecl;
+	VariableDecl *varDecl;
+} Declaration;
+
+typedef enum {
+	// IOD = Increment or Decrement
+	IOD_INCREMENT,
+	IOD_DECREMENT,
+} IncOrDec;
+
+typedef struct {
+	Expression *expr;
+	IncOrDec type;
+} IncDecStat;
+
+typedef struct {
+	s_Expression *expr;
+} ReturnStat;
+
+typedef struct {
+	PrimaryExpr *primary;
+	s_Expression *expr;
+} Assignment;
+
+typedef struct {
+	ReturnStat *retStmt;
+	BreakStat *breakStmt;
+	ContinueStat *conStmt;
+} LeaveStat;
+
+typedef struct {
+	Declaration *decl;
+	LeaveStat *leave;
+	IncDecStat *incDec;
+	Assignment *assignment;
+} UnstructuredStatement;
+
+typedef struct {
+	Block *body;
+} ElseStat;
+
+typedef struct {
+	s_Expression *expr;
+	Block *body;
+	ElseStat *elseStmt;
+} IfStat;
+
+typedef struct {
+	s_Expression *expr;
+	Block *body;
+	LeaveStat *leave;
+} MatchClause;
+
+typedef struct {
+	s_Expression *expr;
+	Vector *clauses;
+} MatchStat;
+
+typedef struct {
+	s_Type *type;
+	char *index;
+	PrimaryExpr *start, *end, *step;
+	Block *body;
+} ForStat;
+
+typedef struct {
+	Block *block;
+	IfStat *ifStmt;
+	MatchStat *matchStmt;
+	ForStat *forStmt;
+} StructuredStatement;
+
+typedef struct {
+	StructuredStatement *structured;
+	UnstructuredStatement *unstructured;
+} Statement;
+
+typedef struct {
+	Vector *stmts;
+} StatementList;
 
 typedef struct s_Type {
 	TypeName *typeName;
 	ArrayType *arrayType;
 	PointerType *pointerType;
-	StructType *structType;
+	StructDecl *structType;
 } Type;
 
 IdentifierList *createIdentifierList();
@@ -128,13 +255,13 @@ FieldDeclList *createFieldDeclList();
 
 void destroyFieldDeclList(FieldDeclList *fieldDeclList);
 
-StructType *createStructType();
+StructDecl *createStructDecl();
 
-void destroyStructType(StructType *structType);
+void destroyStructDecl(StructDecl *structType);
 
-FunctionType *createFunctionType();
+FunctionType *createFunctionSignature();
 
-void destroyFunctionType(FunctionType *funcType);
+void destroyFunctionSignature(FunctionType *funcType);
 
 Type *createType();
 
