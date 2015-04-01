6aa782981dc3af7bb1bef011abb2c1c740ce85e3
Felix Angell <mail@felixangell.com>
2015-04-02 00:01:31 +0100

    removed old code in preperation for LLVM

--- src/sourcefile.c
+++ src/sourcefile.c
@@ -15,31 +15,10 @@ SourceFile *createSourceFile(sds fileName) {
 	return sourceFile;
 }
 
-void writeSourceFile(SourceFile *sourceFile) {
-	sourceFile->generatedSourceName = sdsempty();
-	sourceFile->generatedSourceName = sdscat(sourceFile->generatedSourceName, OUTPUT_PREFIX);
-	sourceFile->generatedSourceName = sdscat(sourceFile->generatedSourceName, sourceFile->name);
-	sourceFile->generatedSourceName = sdscat(sourceFile->generatedSourceName, OUTPUT_EXTENSION);
-
-	verboseModeMessage("Generated source file `%s` as `%s`", sourceFile->name, sourceFile->generatedSourceName);
-	sourceFile->outputFile = fopen(sourceFile->generatedSourceName, "w");
-	if (!sourceFile->outputFile) {
-		perror("fopen: failed to open file");
-		return;
-	}
-}
-
-void closeSourceFile(SourceFile *sourceFile) {
-	fclose(sourceFile->outputFile);
-}
-
 void destroySourceFile(SourceFile *sourceFile) {
-	if (!OUTPUT_C) remove(sourceFile->generatedSourceName);
-
 	verboseModeMessage("Destroyed Source File `%s`", sourceFile->name);
 	sdsfree(sourceFile->fileName);
 	free(sourceFile->name);
-	sdsfree(sourceFile->generatedSourceName);
 	free(sourceFile->alloyFileContents); 
 	free(sourceFile);
 }
