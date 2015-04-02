e1966c1f53b2bb175ccafdd572129db1cd6aafb8
Felix Angell <mail@felixangell.com>
2015-04-02 23:40:00 +0100

    changed back to C, lol

--- includes/util/sourcefile.h
+++ includes/util/sourcefile.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 #include "vector.h"
+#include "headerfile.h"
 #include "util.h"
 
 /**
@@ -13,10 +14,13 @@ typedef struct {
 	sds fileName;				// file name for the source file
 	char* name;					// file name for the source file, raw
 	char* alloyFileContents;	// the contents of the alloy file
+	sds generatedSourceName;	// the generated source name with the _gen_ prefix and .c suffix
 	FILE *outputFile;			// the file output
 
 	Vector *tokens;				// the token stream for the source file
 	Vector *ast;				// the output AST tree
+
+	HeaderFile *headerFile;		// the header file for the source file
 } SourceFile;
 
 /**
@@ -25,6 +29,35 @@ typedef struct {
  * @param fileName the alloy file
  */
 SourceFile *createSourceFile(sds fileName);
+
+/**
+ * Write both the header and source files
+ *
+ * @param sourceFile the sourceFile instance
+ */
+void writeFiles(SourceFile *sourceFile);
+
+/**
+ * Write the source file
+ *
+ * @param sourceFile the sourceFile to write
+ */
+void writeSourceFile(SourceFile *sourceFile);
+
+/**
+ * Close the given sourceFile
+ *
+ * @param sourceFile the sourceFile to close
+ */
+void closeSourceFile(SourceFile *sourceFile);
+
+/**
+ * Close the header and source files
+ *
+ * @param sourceFile the sourceFile instance,
+ * 		  which gives access to the sourceFile & headerFile
+ */
+void closeFiles(SourceFile *sourceFile);
 
 /**
  * Destroy the source file
