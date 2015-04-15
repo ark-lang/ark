c343a39ad0f44b1de145dc52f136767a83b1ae31
Felix Angell <mail@felixangell.com>
2015-04-15 22:48:59 +0100

    finished documenting shit

--- includes/semantic/semantic.h
+++ includes/semantic/semantic.h
@@ -4,6 +4,12 @@
 /**
  * Semantic analysis of our parser, not a high priority as
  * of writing this, so it's partially unimplemented.
+ *
+ * I decided to have multiple hashmaps so we don't have
+ * to create any structs representing the various kinds of
+ * nodes created, thus no memory management... etc
+ *
+ * This is also recursive, like the parser and codegenerator.
  */
 
 #include "ast.h"
@@ -11,23 +17,49 @@
 #include "hashmap.h"
 
 typedef struct {
+	/** the AST to semantically analyze */
 	Vector *parseTree;
 
+	/** hashmap for functions defined */
 	map_t funcSymTable;
 
+	/** hashmap for variables defined */
 	map_t varSymTable;
 } SemanticAnalysis;
 
+/**
+ * Create a new Semantic Analysis instance
+ */
 SemanticAnalysis *createSemanticAnalysis();
 
+/**
+ * Analyzes an unstructured statement top level node
+ */
 void analyzeUnstructuredStatement(SemanticAnalysis *self, UnstructuredStatement *unstructured);
 
+/**
+ * Analyzes a structured statement top level node
+ */
 void analyzeStructuredStatement(SemanticAnalysis *self, StructuredStatement *unstructured);
 
+/**
+ * Analyzes a statement top level node
+ */
 void analyzeStatement(SemanticAnalysis *self, Statement *stmt);
 
+/**
+ * Start semantically analyzing the source files. 
+ *
+ * NOTE TO SELF FELIKS:
+ * I have a feeling the order of source files being analyzed will matter
+ * especially if we're doing this recursively... so I should probably think
+ * about this.
+ */
 void startSemanticAnalysis(SemanticAnalysis *self);
 
+/**
+ * Destroys the semantic analysis instance given
+ */
 void destroySemanticAnalysis(SemanticAnalysis *self);
 
 #endif // __SEMANTIC_H
