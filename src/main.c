92283aae1df91f3a27ddd84ffee331df85f149c5
Felix Angell <mail@felixangell.com>
2015-04-17 18:54:05 +0100

    setup for type inference

--- src/main.c
+++ src/main.c
@@ -16,11 +16,11 @@ int main(int argc, char** argv) {
 	// finished timer
 	timer = clock() - timer;	// calculate time taken
 
-	// calculate seconds and milliseconds taken
-	double secondsTaken = ((double) timer) / CLOCKS_PER_SEC;
-	double msTaken = secondsTaken * 1000;
-
 	if (alloyc) {
+		// calculate seconds and milliseconds taken
+		double secondsTaken = ((double) timer) / CLOCKS_PER_SEC;
+		double msTaken = secondsTaken * 1000;
+
 		primaryMessage("Finished in %.6f/s (%.0f/ms)", secondsTaken, msTaken);
 	}
 	
