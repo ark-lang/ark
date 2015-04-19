725828e3c7fba7fc7e1675822f4f70b3b7e5f202
Felix Angell <mail@felixangell.com>
2015-04-19 11:49:08 +0100

    fixed small errors

--- src/main.c
+++ src/main.c
@@ -3,24 +3,23 @@
 #include "alloyc.h"
 
 int main(int argc, char** argv) {
-	// start the timer
-	clock_t timer = clock();
 
 	// alloy compiler stuff
 	AlloyCompiler *alloyc = createAlloyCompiler(argc, argv);
 	if (alloyc) {
+		// start the timer
+		clock_t timer = clock();
 		startAlloyCompiler(alloyc);
 		destroyAlloyCompiler(alloyc);
-	}
 
-	// finished timer
-	timer = clock() - timer;	// calculate time taken
+		// finished timer
+		timer = clock() - timer;	// calculate time taken
 
-	if (alloyc) {
 		// calculate seconds and milliseconds taken
 		double secondsTaken = ((double) timer) / CLOCKS_PER_SEC;
 		double msTaken = secondsTaken * 1000;
 
+		// write the message
 		primaryMessage("Finished in %.6f/s (%.0f/ms)", secondsTaken, msTaken);
 	}
 	
