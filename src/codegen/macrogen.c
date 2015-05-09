b7795e60a13300bcf4589a5b40fbc441626efa03
MovingtoMars <liam@bumblebee.net.nz>
2015-05-09 12:14:48 +1200

    rename LINKER_FLAGS to use camelCase

--- src/codegen/macrogen.c
+++ src/codegen/macrogen.c
@@ -21,9 +21,9 @@ void emitLinkerFlagMacro(CodeGenerator *self, LinkerFlagMacro *linker) {
 	memcpy(temp, &linker->flag[1], len - 2);
 	temp[len - 2] = '\0';
 
-	self->LINKER_FLAGS = sdscat(self->LINKER_FLAGS, " -l");
-	self->LINKER_FLAGS = sdscat(self->LINKER_FLAGS, temp);
-	self->LINKER_FLAGS = sdscat(self->LINKER_FLAGS, " ");
+	self->linkerFlags = sdscat(self->linkerFlags, " -l");
+	self->linkerFlags = sdscat(self->linkerFlags, temp);
+	self->linkerFlags = sdscat(self->linkerFlags, " ");
 }
 
 void emitMacroNode(CodeGenerator *self, Macro *macro) {
