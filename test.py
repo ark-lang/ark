ae4bd30437718ab0d774ab05ded6776ddded4ab0
Felix Angell <mail@felixangell.com>
2015-05-14 09:23:32 +0100

    type inference for basic numbers

--- test.py
+++ test.py
@@ -5,6 +5,9 @@
 
 import os, subprocess, sys
 
+# Class representing our test file
+# has its name and whether it failed
+# or not
 class TestFile:
 	name = "?"
 	failed = True
@@ -13,24 +16,39 @@ class TestFile:
 		self.name = name
 		self.failed = failed
 
+# all of the files tested go here
 files_tested = []
+
+# how many files passed
 num_of_files_passed = 0
+
+# how many files failed
 num_of_files_failed = 0
+
+# if we show the ouput to the console
+# TODO maybe print out summary even if 
+# no output is shown?
 show_output = False
 
+# This is kind of hacky and not very scalable,
+# will show the output if they added -o as an arg
 if len(sys.argv) > 1 and str(sys.argv[1]) == "-o":
 	show_output = True
 
+# function for bolding a string
 def bold(s):
 	return "\033[1m" + s + "\033[0m"
 
+# function for making a string red
 def red(s):
 	return "\x1B[31m" + s + "\x1B[00m"
 
+# function for making a string green
 def green(s):
 	return "\x1B[32m" + s + "\x1B[00m"
 
 for name in os.listdir("tests"):
+	# test all of the files that end with _test.aly
 	if name.endswith("_test.aly"):
 		output_file = name + ".test"
 		
@@ -71,6 +89,7 @@ for name in os.listdir("tests"):
 			files_tested.append(TestFile(output_file, False))
 			num_of_files_passed += 1
 
+# print results
 total_num_of_files = num_of_files_passed + num_of_files_failed
 print(bold("Results (" + str(num_of_files_passed) + "/" + str(total_num_of_files) + ") files passed: ")) # some margin
 
