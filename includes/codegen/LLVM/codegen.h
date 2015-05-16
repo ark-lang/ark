2479e9be98b6b21938af0a049412c9546eea42ea
MovingtoMars <liam@bumblebee.net.nz>
2015-05-16 18:04:42 +1200

    add LLVM codegen skeleton

---
+++ includes/codegen/LLVM/codegen.h
@@ -0,0 +1,59 @@
+#ifndef __LLVM_CODEGEN_H
+#define __LLVM_CODEGEN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "util.h"
+#include "parser.h"
+#include "vector.h"
+#include "hashmap.h"
+
+typedef struct {
+	/**
+	 * The current abstract syntax tree being
+	 * generated for
+	 */
+	Vector *abstractSyntaxTree;
+	
+	/**
+	 * All of the source files we're generating code
+	 * for
+	 */
+	Vector *sourceFiles;
+
+	/**
+	 * The current source file to
+	 * generate code for
+	 */
+	SourceFile *currentSourceFile;
+
+	/**
+	 * Our index in the abstract syntax
+	 * tree
+	 */
+	int currentNode;
+} LLVMCodeGenerator;
+
+/**
+ * Creates an instance of the code generator
+ * @param  sourceFiles the source files to codegen for
+ * @return             the instance we created
+ */
+LLVMCodeGenerator *createLLVMCodeGenerator(Vector *sourceFiles);
+
+/**
+ * This is pretty much where the magic happens, this will
+ * start the code gen
+ * @param self the code gen instance
+ */
+void startLLVMCodeGeneration(LLVMCodeGenerator *self);
+
+/**
+ * Destroys the given code gen instance
+ * @param self the code gen instance
+ */
+void destroyLLVMCodeGenerator(LLVMCodeGenerator *self);
+
+#endif
