1cccdd545a222811747d1bae32e4b13a0ad413aa
Felix Angell <mail@felixangell.com>
2015-05-21 13:43:31 +0100

    more cleanups

--- includes/util/sourcefile.h
+++ includes/util/sourcefile.h
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 
 #include "vector.h"
-#include "headerfile.h"
 #include "util.h"
 #include "parser.h"
 
@@ -19,13 +18,9 @@ typedef struct {
 	sds fileName;            // file name for the source file
 	char *name;              // file name for the source file, raw
 	char *fileContents;      // the contents of the file
-	sds generatedSourceName; // the generated source name with the _gen_ prefix and .c suffix
-	FILE *outputFile;        // the file output
     LLVMModuleRef module;
 	Vector *tokens;          // the token stream for the source file
 	Vector *ast;             // the output AST tree
-
-	HeaderFile *headerFile;  // the header file for the source file
 } SourceFile;
 
 /**
@@ -33,31 +28,6 @@ typedef struct {
  * @param fileName the file name
  */
 SourceFile *createSourceFile(sds fileName);
-
-/**
- * Write both the header and source files
- * @param sourceFile the sourceFile instance
- */
-void writeFiles(SourceFile *sourceFile);
-
-/**
- * Write the source file
- * @param sourceFile the sourceFile to write
- */
-void writeSourceFile(SourceFile *sourceFile);
-
-/**
- * Close the given source file
- * @param sourceFile the sourceFile to close
- */
-void closeSourceFile(SourceFile *sourceFile);
-
-/**
- * Close the header and source files
- * @param sourceFile the sourceFile instance,
- * 		  which gives access to the sourceFile & headerFile
- */
-void closeFiles(SourceFile *sourceFile);
 
 /**
  * Destroy the source file
