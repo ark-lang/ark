9d255759bccc43780b2e777bbd769403ba933796
Felix Angell <mail@felixangell.com>
2015-05-21 13:43:31 +0100

    more cleanups

--- src/util/sourcefile.c
+++ src/util/sourcefile.c
@@ -3,7 +3,6 @@
 SourceFile *createSourceFile(sds fileName) {
 	SourceFile *sourceFile = malloc(sizeof(*sourceFile));
 	sourceFile->fileName = fileName;
-	sourceFile->headerFile = createHeaderFile(fileName);
 	sourceFile->name = getFileName(sourceFile->fileName);
 	sourceFile->fileContents = readFile(fileName);
 	sourceFile->tokens = NULL;
@@ -18,42 +17,12 @@ SourceFile *createSourceFile(sds fileName) {
 	return sourceFile;
 }
 
-void writeFiles(SourceFile *sourceFile) {
-	writeSourceFile(sourceFile);
-	writeHeaderFile(sourceFile->headerFile);
-}
-
-void writeSourceFile(SourceFile *sourceFile) {
-	sourceFile->generatedSourceName = sdsempty();
-	sourceFile->generatedSourceName = sdscat(sourceFile->generatedSourceName, "_gen_");
-	sourceFile->generatedSourceName = sdscat(sourceFile->generatedSourceName, sourceFile->name);
-	sourceFile->generatedSourceName = sdscat(sourceFile->generatedSourceName, ".c");
-
-	verboseModeMessage("Generated source file `%s` as `%s`", sourceFile->name, sourceFile->generatedSourceName);
-	sourceFile->outputFile = fopen(sourceFile->generatedSourceName, "w");
-	if (!sourceFile->outputFile) {
-		perror("fopen: failed to open file");
-		return;
-	}
-}
-
-void closeFiles(SourceFile *sourceFile) {
-	closeSourceFile(sourceFile);
-	closeHeaderFile(sourceFile->headerFile);
-}
-
-void closeSourceFile(SourceFile *sourceFile) {
-	fclose(sourceFile->outputFile);
-}
-
 void destroySourceFile(SourceFile *sourceFile) {
-	remove(sourceFile->generatedSourceName);
-
-	destroyHeaderFile(sourceFile->headerFile);
-
 	verboseModeMessage("Destroyed source file `%s`", sourceFile->name);
+
 	if (sourceFile->tokens) destroyVector(sourceFile->tokens); // TODO destroy elements in tokens
 	if (sourceFile->ast) destroyParseTree(sourceFile->ast);
+
 	sdsfree(sourceFile->fileName);
 	free(sourceFile->name);
 	free(sourceFile->fileContents); 
