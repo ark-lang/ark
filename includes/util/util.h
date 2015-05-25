ad99bbf32a764f589f284fb269848279f9b7a734
Felix Angell <mail@felixangell.com>
2015-05-25 14:48:06 +0100

    fixed arguments and various memory leaks

--- includes/util/util.h
+++ includes/util/util.h
@@ -28,9 +28,11 @@
 #ifdef WINDOWS
 	#define GET_RED_TEXT(x) (x)
 	#define GET_ORANGE_TEXT(x) (x)
+    #define BOLD_TEXT(x) (x)
 #else
 	#define GET_RED_TEXT(x) ("\x1B[31m" x "\x1B[00m")
 	#define GET_ORANGE_TEXT(x) ("\x1B[33m" x "\x1B[00m")
+    #define BOLD_TEXT(x) ("\x1B[01m" x "\x1B[00m")
 #endif
 
 /** macro for array length */
