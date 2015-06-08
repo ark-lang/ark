5e2dcc8590dda5326524de76c0cf5d9a988d08f3
MovingtoMars <liam@bumblebee.net.nz>
2015-06-08 20:11:45 +1200

    lots more work on docgen

--- doc/decl.go
+++ doc/decl.go
@@ -1,8 +1,76 @@
 package doc
 
-import "github.com/ark-lang/ark/parser"
+import (
+	"html/template"
+
+	"github.com/ark-lang/ark/parser"
+)
 
 type Decl struct {
-	AstDecl parser.Decl
-	Docs    string
+	Node       parser.Decl
+	Docs       string
+	ParsedDocs template.HTML // docs after markdown parsing
+	Ident      string        // identifier
+	Snippet    string        // code snippet of declaration
+}
+
+func (v *Decl) process() {
+	v.ParsedDocs = template.HTML(parseMarkdown(v.Docs))
+
+	switch v.Node.(type) {
+	case *parser.FunctionDecl:
+		v.Ident, v.Snippet = generateFunctionDeclSnippet(v.Node.(*parser.FunctionDecl))
+	case *parser.StructDecl:
+		v.Ident, v.Snippet = generateStructDeclSnippet(v.Node.(*parser.StructDecl))
+	case *parser.VariableDecl:
+		v.Ident, v.Snippet = generateVariableDeclSnippet(v.Node.(*parser.VariableDecl))
+	default:
+		panic("unimplimented decl type in doc")
+	}
+}
+
+func generateFunctionDeclSnippet(decl *parser.FunctionDecl) (ident, snippet string) {
+	ident = decl.Function.Name
+
+	snippet = parser.KEYWORD_FUNC + " " + ident + "("
+	for i, par := range decl.Function.Parameters {
+		snippet += par.Variable.Name + ": " + par.Variable.Type.TypeName()
+		if i < len(decl.Function.Parameters)-1 {
+			snippet += ", "
+		}
+	}
+	snippet += ")"
+	if decl.Function.ReturnType != nil {
+		snippet += ": " + decl.Function.ReturnType.TypeName()
+
+	}
+
+	return
+}
+
+func generateStructDeclSnippet(decl *parser.StructDecl) (ident, snippet string) {
+	ident = decl.Struct.Name
+	snippet = "struct " + decl.Struct.Name + " {"
+	for i, member := range decl.Struct.Variables {
+		snippet += "\n    " + member.Variable.Name + ": " + member.Variable.Type.TypeName()
+		if member.Assignment != nil {
+			snippet += " = [TODO values]"
+		}
+		if i < len(decl.Struct.Variables)-1 {
+			snippet += ","
+		} else {
+			snippet += "\n"
+		}
+	}
+	snippet += "}"
+	return
+}
+
+func generateVariableDeclSnippet(decl *parser.VariableDecl) (ident, snippet string) {
+	ident = decl.Variable.Name
+	snippet = ident + ": " + decl.Variable.Type.TypeName()
+	if decl.Assignment != nil {
+		snippet += " = [TODO values]"
+	}
+	return
 }
--- doc/doc.go
+++ doc/doc.go
@@ -38,20 +38,33 @@ func (v *Docgen) Generate(verbose bool) {
 
 func (v *Docgen) traverse(verbose bool) {
 	for _, file := range v.Input {
-		v.curOutput = &File{Name: file.Name}
+		v.curOutput = &File{
+			Name: file.Name,
+		}
 
 		for _, n := range file.Nodes {
 			switch n.(type) {
 			case parser.Decl:
 				decl := &Decl{
-					AstDecl: n.(parser.Decl),
+					Node: n.(parser.Decl),
 				}
 
-				for _, comm := range decl.AstDecl.DocComments() {
+				for _, comm := range decl.Node.DocComments() {
 					decl.Docs += comm.Contents + "\n"
 				}
 
-				v.curOutput.Decls = append(v.curOutput.Decls, decl)
+				decl.process()
+
+				switch n.(type) {
+				case *parser.FunctionDecl:
+					v.curOutput.FunctionDecls = append(v.curOutput.FunctionDecls, decl)
+				case *parser.StructDecl:
+					v.curOutput.StructDecls = append(v.curOutput.StructDecls, decl)
+				case *parser.VariableDecl:
+					v.curOutput.VariableDecls = append(v.curOutput.VariableDecls, decl)
+				default:
+					panic("dammit")
+				}
 			}
 		}
 
@@ -61,23 +74,19 @@ func (v *Docgen) traverse(verbose bool) {
 }
 
 func (v *Docgen) generate() {
-	outputDir := "./" + v.Dir + "/"
+	if v.Dir[len(v.Dir)-1] != '/' {
+		v.Dir += "/"
+	}
 
-	err := os.MkdirAll(outputDir, os.ModeDir|0777)
+	err := os.MkdirAll(v.Dir+"files", os.ModeDir|0777)
 	if err != nil {
 		panic(err)
 	}
 
-	indexFile, err := os.OpenFile(outputDir+"index.html", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
-	if err != nil {
-		panic(err)
+	v.generateStyle()
+	v.generateIndex()
+
+	for _, outputFile := range v.output {
+		v.generateFile(outputFile)
 	}
-	defer indexFile.Close()
-	v.generateIndex(indexFile)
-
-	/*for _, outputFile := range v.output {
-		for _, decl := range outputFile.Decls {
-
-		}
-	}*/
 }
