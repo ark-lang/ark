e4e8cf3298274e6ff1283672e6fcec7fa2695bcd
Felix Angell <mail@felixangell.com>
2015-06-08 12:12:47 +0100

    fancy styles

--- doc/style.go
+++ doc/style.go
@@ -18,10 +18,79 @@ const STYLE = `
 * {
 	margin: 0;
 	padding: 0;
+    box-sizing: border-box;
 }
 
-a, p, h1, h2, h3, h4, h5, h6 {
-	font-family: "Helvetica Neue", sans-serif;
+html {
+	overflow-y: scroll;
+}
+
+body {
+    font-family: "Source Serif Pro", Georgia, Times New Roman, serif;
+}
+
+body > .wrapper {
+	background: #eee;
+	margin-top: 20px;
+}
+
+.wrapper {
+	display: block;
+	width: 980px;
+	margin: 0 auto;
+	padding: 20px;
+}
+
+a {
+	font-family: "Fira Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+.slab a {
+	color: #fff;
+	font-weight: 800;
+}
+
+.slab a:hover {
+	color: #eee;
+}
+
+a {
+	color: #24424F;
+	text-decoration: none;
+}
+
+a:hover {
+	color: #112833;
+	text-decoration: underline;
+}
+
+section.doc {
+	padding: 20px;
+}
+
+.slab {
+	display: block;
+	width: 100%;
+	background: #546E7A;
+	padding: 6rem;
+	color: #fff;
+}
+
+.slab-title {
+	font-size: 3em;
+	color: #fff;
+}
+
+ul {
+	margin-left: 20px;
+}
+
+h1, h2, h3, h4, h6 {
+    font-family: "Fira Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+pre, code {
+    font-family: "Fira Mono", monospace;
 }
 
 a {
