755f888a6ba8e2c25c4c1040c7670339a1fca8c8
MovingtoMars <liam@bumblebee.net.nz>
2015-06-09 17:28:43 +1200

    add negative int lits

--- parser/unoptype_string.go
+++ parser/unoptype_string.go
@@ -4,9 +4,9 @@ package parser
 
 import "fmt"
 
-const _UnOpType_name = "UNOP_ERRUNOP_LOG_NOTUNOP_BIT_NOTUNOP_ADDRESS"
+const _UnOpType_name = "UNOP_ERRUNOP_LOG_NOTUNOP_BIT_NOTUNOP_NEGATIVEUNOP_ADDRESS"
 
-var _UnOpType_index = [...]uint8{8, 20, 32, 44}
+var _UnOpType_index = [...]uint8{8, 20, 32, 45, 57}
 
 func (i UnOpType) String() string {
 	if i < 0 || i >= UnOpType(len(_UnOpType_index)) {
