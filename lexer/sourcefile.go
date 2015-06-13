24a14baa5b900fa37f6f7e77db3d446c1151335e
MovingtoMars <liam@bumblebee.net.nz>
2015-06-13 19:54:47 +1200

    move common/sourcefile to lexer/sourcefile

--- lexer/sourcefile.go
+++ lexer/sourcefile.go
@@ -1,15 +1,13 @@
-package common
+package lexer
 
 import (
 	"io/ioutil"
-
-	"github.com/ark-lang/ark/lexer"
 )
 
 type Sourcefile struct {
 	Filename string
 	Contents []rune
-	Tokens   []*lexer.Token
+	Tokens   []*Token
 }
 
 func NewSourcefile(file string) (*Sourcefile, error) {
