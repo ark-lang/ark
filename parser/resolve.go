90bc8e5ee05349df2b9724bf0ef93c2da4c87a91
MovingtoMars <liam@bumblebee.net.nz>
2015-06-20 20:22:26 +1200

    comments on resolve.go

--- parser/resolve.go
+++ parser/resolve.go
@@ -1,5 +1,12 @@
 package parser
 
+// What is done:
+// - variables
+// - function
+// What is not:
+// - types
+// - traits
+
 type unresolvedName struct {
 	moduleNames []string
 	name        string
@@ -12,54 +19,6 @@ func (v unresolvedName) String() string {
 	}
 	return ret + v.name
 }
-
-/*func (v *semanticAnalyzer) resolveAll() {
-	for _, n := range v.unresolvedNodes {
-		switch n.(type) {
-		case *CallExpr:
-			v.resolveCallExpr(n.(*CallExpr))
-		case *CastExpr:
-			v.resolveCastExpr(n.(*CastExpr))
-		case *VariableDecl:
-			v.resolveVariableDecl(n.(*VariableDecl))
-		case *FunctionDecl:
-			v.resolveFunctionDecl(n.(*FunctionDecl))
-		}
-	}
-}
-
-func (v *semanticAnalyzer) resolveCallExpr(n *CallExpr) {
-	n.Function = v.module.GlobalScope.GetFunction(n.functionName)
-
-	if n.Function == nil {
-		v.err(n, "Unresolved function `%s`", n.functionName)
-	}
-}
-
-func (v *semanticAnalyzer) resolveCastExpr(n *CastExpr) {
-	n.Type = getTypeFromString(v.module.GlobalScope, n.typeName)
-
-	if n.Type == nil {
-		v.err(n, "Unresolved type `%s`", n.typeName)
-	}
-}
-
-func (v *semanticAnalyzer) resolveVariableDecl(n *VariableDecl) {
-	n.Variable.Type = getTypeFromString(v.module.GlobalScope, n.Variable.typeName)
-
-	if n.Variable.Type == nil {
-		v.err(n, "Unresolved type `%s`", n.Variable.typeName)
-	}
-}
-
-func (v *semanticAnalyzer) resolveFunctionDecl(n *FunctionDecl) {
-	n.Function.ReturnType = getTypeFromString(v.module.GlobalScope, n.Function.returnTypeName)
-
-	if n.Function.ReturnType == nil {
-		v.err(n, "Unresolved type `%s`", n.Function.returnTypeName)
-	}
-}
-*/
 
 func (v *semanticAnalyzer) errResolve(thing Locatable, name unresolvedName) {
 	v.err(thing, "Cannot resolve `%s`", name.String())
