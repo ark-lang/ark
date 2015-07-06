37121118a9137b8bdeaf4299c6c80f2728bffe3e
Felix Angell <mail@felixangell.com>
2015-07-06 13:12:11 +0100

    every module gets a private c module, closes #496

--- codegen/LLVMCodegen/codegen.go
+++ codegen/LLVMCodegen/codegen.go
@@ -146,6 +146,8 @@ func (v *Codegen) Generate(input []*parser.Module, modules map[string]*parser.Mo
 			fmt.Printf(util.TEXT_BOLD+util.TEXT_GREEN+"Finished codegenning "+util.TEXT_RESET+infile.Name+" (%.2fms)\n",
 				float32(dur.Nanoseconds())/1000000)
 		}
+
+		v.curFile.Module.Dump()
 	}
 
 	passManager.Dispose()
