cd06af15f115273d6357972bd145d126643237b5
MovingtoMars <liam@bumblebee.net.nz>
2015-07-08 18:46:47 +1200

    output types

--- src/cmd/ark/args.go
+++ src/cmd/ark/args.go
@@ -1,6 +1,9 @@
 package main
 
-import "gopkg.in/alecthomas/kingpin.v2"
+import (
+	"github.com/ark-lang/ark/src/codegen/LLVMCodegen"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
 
 type inputList []string
 
@@ -24,15 +27,32 @@ var (
 	logLevel = app.Flag("loglevel", "Set the level of logging to show").Default("info").Enum("debug", "verbose", "info", "warning", "error")
 	logTags  = app.Flag("logtags", "Which log tags to show").Default("all").String()
 
-	buildCom     = app.Command("build", "Build an executable.")
-	buildOutput  = buildCom.Flag("output", "Output binary name.").Short('o').Default("main").String()
-	buildInputs  = newInputList(buildCom.Arg("input", "Ark source files."))
-	buildCodegen = buildCom.Flag("codegen", "Codegen backend to use").Default("llvm").Enum("none", "llvm")
-	buildStatic  = buildCom.Flag("static", "Pass the -static option to cc.").Bool()
-	buildRun     = buildCom.Flag("run", "Run the executable.").Bool()
-	buildAsm     = buildCom.Flag("output-assembly", "Stop compiling before assembling.").Short('S').Bool()
+	buildCom        = app.Command("build", "Build an executable.")
+	buildOutput     = buildCom.Flag("output", "Output binary name.").Short('o').Default("main").String()
+	buildInputs     = newInputList(buildCom.Arg("input", "Ark source files."))
+	buildCodegen    = buildCom.Flag("codegen", "Codegen backend to use").Default("llvm").Enum("none", "llvm")
+	buildStatic     = buildCom.Flag("static", "Pass the -static option to cc.").Bool()
+	buildRun        = buildCom.Flag("run", "Run the executable.").Bool()
+	buildOutputType = buildCom.Flag("output-type", "Codegen backend to use").Default("executable").Enum("executable", "assembly", "object", "llvm-ir", "llvm-bc")
 
 	docgenCom    = app.Command("docgen", "Generate documentation.")
 	docgenDir    = docgenCom.Flag("dir", "Directory to place generated docs in.").Default("docgen").String()
 	docgenInputs = newInputList(docgenCom.Arg("input", "Ark source files."))
 )
+
+func parseOutputType(name string) LLVMCodegen.OutputType {
+	switch name {
+	case "executable":
+		return LLVMCodegen.OUTPUT_EXECUTABLE
+	case "assembly":
+		return LLVMCodegen.OUTPUT_ASSEMBLY
+	case "object":
+		return LLVMCodegen.OUTPUT_OBJECT
+	case "llvm-ir":
+		return LLVMCodegen.OUTPUT_LLVM_IR
+	case "llvm-bc":
+		return LLVMCodegen.OUTPUT_LLVM_BC
+	default:
+		panic("unimplemented output type")
+	}
+}
---
+++ src/codegen/LLVMCodegen/binary.go
@@ -0,0 +1,164 @@
+package LLVMCodegen
+
+import (
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/ark-lang/ark/src/parser"
+	"llvm.org/llvm/bindings/go/llvm"
+)
+
+type OutputType int
+
+const (
+	OUTPUT_ASSEMBLY OutputType = iota
+	OUTPUT_OBJECT
+	OUTPUT_LLVM_IR
+	OUTPUT_LLVM_BC
+	OUTPUT_EXECUTABLE
+)
+
+func (v *Codegen) createBitcode(file *parser.Module) string {
+	filename := file.Name + ".bc"
+
+	fileHandle, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		v.err("Couldn't create bitcode file "+filename+": `%s`", err.Error())
+	}
+	defer fileHandle.Close()
+
+	if err := llvm.WriteBitcodeToFile(file.Module, fileHandle); err != nil {
+		v.err("failed to write bitcode to file for "+file.Name+": `%s`", err.Error())
+	}
+
+	return filename
+}
+
+func (v *Codegen) bitcodeToASM(filename string) string {
+	asmName := filename
+
+	if strings.HasSuffix(filename, ".bc") {
+		asmName = asmName[:len(asmName)-3]
+	}
+
+	asmName += ".s"
+
+	cmd := exec.Command("llc", filename, "-o", asmName)
+	if out, err := cmd.CombinedOutput(); err != nil {
+		v.err("Failed to convert bitcode to assembly: `%s`\n%s", err.Error(), string(out))
+	}
+
+	return asmName
+}
+
+func (v *Codegen) asmToObject(filename string) string {
+	objName := filename
+
+	if strings.HasSuffix(filename, ".s") {
+		objName = objName[:len(objName)-2]
+	}
+
+	objName += ".o"
+
+	if v.Compiler == "" {
+		v.Compiler = "cc"
+	}
+
+	args := append(v.CompilerArgs, "-fno-PIE", "-c", filename, "-o", objName)
+
+	cmd := exec.Command(v.Compiler, args...)
+	if out, err := cmd.CombinedOutput(); err != nil {
+		v.err("Failed to convert assembly to object file: `%s`\n%s", err.Error(), string(out))
+	}
+
+	return objName
+}
+
+func (v *Codegen) createIR(mod *parser.Module) string {
+	filename := mod.Name + ".ll"
+
+	err := ioutil.WriteFile(filename, []byte(mod.Module.String()), 0666)
+	if err != nil {
+		v.err("Couldn't write IR file "+filename+": `%s`", err.Error())
+	}
+
+	return filename
+}
+
+func (v *Codegen) createBinary() {
+	// god this is a long and ugly function
+
+	if v.OutputType == OUTPUT_LLVM_IR {
+		for _, file := range v.input {
+			v.createIR(file)
+		}
+		return
+	}
+
+	linkArgs := append(v.LinkerArgs, "-fno-PIE", "-nodefaultlibs", "-lc", "-lm")
+
+	bitcodeFiles := []string{}
+
+	for _, file := range v.input {
+		bitcodeFiles = append(bitcodeFiles, v.createBitcode(file))
+	}
+
+	if v.OutputType == OUTPUT_LLVM_BC {
+		return
+	}
+
+	asmFiles := []string{}
+
+	for _, name := range bitcodeFiles {
+		asmName := v.bitcodeToASM(name)
+		asmFiles = append(asmFiles, asmName)
+	}
+
+	for _, bc := range bitcodeFiles {
+		if err := os.Remove(bc); err != nil {
+			v.err("Failed to remove "+bc+": `%s`", err.Error())
+		}
+	}
+
+	if v.OutputType == OUTPUT_ASSEMBLY {
+		return
+	}
+
+	objFiles := []string{}
+
+	for _, asmFile := range asmFiles {
+		objName := v.asmToObject(asmFile)
+
+		objFiles = append(objFiles, objName)
+		linkArgs = append(linkArgs, objName)
+	}
+
+	for _, asmFile := range asmFiles {
+		os.Remove(asmFile)
+	}
+
+	if v.OutputType == OUTPUT_OBJECT {
+		return
+	}
+
+	if v.OutputName == "" {
+		panic("OutputName is empty")
+	}
+
+	linkArgs = append(linkArgs, "-o", v.OutputName)
+
+	if v.Linker == "" {
+		v.Linker = "cc"
+	}
+
+	cmd := exec.Command(v.Linker, linkArgs...)
+	if out, err := cmd.CombinedOutput(); err != nil {
+		v.err("failed to link object files: `%s`\n%s", err.Error(), string(out))
+	}
+
+	for _, objFile := range objFiles {
+		os.Remove(objFile)
+	}
+}
