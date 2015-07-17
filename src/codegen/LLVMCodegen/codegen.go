a8051881463d2c3775df3996917e268d943c3d80
Felix Angell <mail@felixangell.com>
2015-07-17 03:51:58 +0100

    removed module decls from parser

--- src/codegen/LLVMCodegen/codegen.go
+++ src/codegen/LLVMCodegen/codegen.go
@@ -409,8 +409,6 @@ func (v *Codegen) genDecl(n parser.Decl) {
 		// todo
 	case *parser.VariableDecl:
 		v.genVariableDecl(n.(*parser.VariableDecl), true)
-	case *parser.ModuleDecl:
-		// TODO!!
 	default:
 		v.err("unimplimented decl found: `%s`", n.NodeName())
 	}
