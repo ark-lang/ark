adfdbd8c5c34c61de11bfc379a886ee1b6d65b34
Emil Lauridsen <mine809@gmail.com>
2015-07-28 17:15:12 +0200

    Semantic check for use of variable before declaration.
    Resolves #568

---
+++ src/parser/checks/use_before_declare.go
@@ -0,0 +1,44 @@
+package checks
+
+import (
+	"github.com/ark-lang/ark/src/parser"
+)
+
+type UseBeforeDeclareCheck struct {
+	scopes []map[string]bool
+	scope  map[string]bool
+}
+
+func (v *UseBeforeDeclareCheck) EnterScope(s *SemanticAnalyzer) {
+	lastScope := v.scope
+	if v.scope != nil {
+		v.scopes = append(v.scopes, v.scope)
+	}
+
+	v.scope = make(map[string]bool)
+	for name, declared := range lastScope {
+		v.scope[name] = declared
+	}
+}
+func (v *UseBeforeDeclareCheck) ExitScope(s *SemanticAnalyzer) {
+	v.scope = nil
+	if len(v.scopes) > 0 {
+		idx := len(v.scopes) - 1
+		v.scope, v.scopes[idx] = v.scopes[idx], nil
+		v.scopes = v.scopes[:idx]
+	}
+}
+
+func (v *UseBeforeDeclareCheck) Visit(s *SemanticAnalyzer, n parser.Node) {
+	switch n.(type) {
+	case *parser.VariableDecl:
+		decl := n.(*parser.VariableDecl)
+		v.scope[decl.Variable.Name] = true
+
+	case *parser.VariableAccessExpr:
+		expr := n.(*parser.VariableAccessExpr)
+		if !v.scope[expr.Variable.Name] {
+			s.Err(expr, "Use of variable before declaration: %s", expr.Variable.Name)
+		}
+	}
+}
