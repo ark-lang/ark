e7a66ed2f4fedde4de82b2681644842f9bf18a40
Felix Angell <mail@felixangell.com>
2015-08-12 00:53:50 +0100

    fixed copy pasta error in help

--- src/cmd/ark/args.go
+++ src/cmd/ark/args.go
@@ -33,7 +33,7 @@ var (
 	buildCodegen    = buildCom.Flag("codegen", "Codegen backend to use").Default("llvm").Enum("none", "llvm")
 	buildStatic     = buildCom.Flag("static", "Pass the -static option to cc.").Bool()
 	buildRun        = buildCom.Flag("run", "Run the executable.").Bool()
-	buildOutputType = buildCom.Flag("output-type", "Codegen backend to use").Default("executable").Enum("executable", "assembly", "object", "llvm-ir", "llvm-bc")
+	buildOutputType = buildCom.Flag("output-type", "The format to produce after code generation").Default("executable").Enum("executable", "assembly", "object", "llvm-ir", "llvm-bc")
 	buildOptLevel   = buildCom.Flag("opt-level", "LLVM optimization level").Short('O').Default("0").Int()
 	buildOwnership  = buildCom.Flag("ownership", "Do ownership checks").Bool()
 
