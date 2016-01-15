7dc7528f5d410faf1d188ea86b9942240238770e
MovingtoMars <liam@bumblebee.net.nz>
2016-01-15 19:17:00 +1300

    err if break/next not in loop

--- src/semantic/break_next.go
+++ src/semantic/break_next.go
@@ -1,19 +1,43 @@
 package semantic
 
-import "github.com/ark-lang/ark/src/parser"
+import (
+	"github.com/ark-lang/ark/src/parser"
+	"github.com/ark-lang/ark/src/util"
+)
 
 // TODO handle match/switch, if we need to
 
 type BreakAndNextCheck struct {
+	nestedLoopCount map[*parser.Function]int
+	functions       []*parser.Function
 }
 
-func (v *BreakAndNextCheck) Init(s *SemanticAnalyzer)                     {}
-func (v *BreakAndNextCheck) EnterScope(s *SemanticAnalyzer)               {}
-func (v *BreakAndNextCheck) ExitScope(s *SemanticAnalyzer)                {}
-func (v *BreakAndNextCheck) PostVisit(s *SemanticAnalyzer, n parser.Node) {}
-func (v *BreakAndNextCheck) Destroy(s *SemanticAnalyzer)                  {}
+func (v *BreakAndNextCheck) Init(s *SemanticAnalyzer) {
+	v.nestedLoopCount = make(map[*parser.Function]int)
+}
+
+func (v *BreakAndNextCheck) EnterScope(s *SemanticAnalyzer) {}
+func (v *BreakAndNextCheck) ExitScope(s *SemanticAnalyzer)  {}
+func (v *BreakAndNextCheck) Destroy(s *SemanticAnalyzer)    {}
 
 func (v *BreakAndNextCheck) Visit(s *SemanticAnalyzer, n parser.Node) {
+	switch n := n.(type) {
+	case *parser.NextStat, *parser.BreakStat:
+		if v.nestedLoopCount[v.functions[len(v.functions)-1]] == 0 {
+			s.Err(n, "%s must be in a loop", util.CapitalizeFirst(n.NodeName()))
+		}
+
+	case *parser.LoopStat:
+		v.nestedLoopCount[v.functions[len(v.functions)-1]]++
+
+	case *parser.FunctionDecl:
+		v.functions = append(v.functions, n.Function)
+	case *parser.LambdaExpr:
+		v.functions = append(v.functions, n.Function)
+	}
+}
+
+func (v *BreakAndNextCheck) PostVisit(s *SemanticAnalyzer, n parser.Node) {
 	switch n := n.(type) {
 	case *parser.Block:
 		for i, c := range n.Nodes {
@@ -21,6 +45,15 @@ func (v *BreakAndNextCheck) Visit(s *SemanticAnalyzer, n parser.Node) {
 				s.Err(n.Nodes[i+1], "Unreachable code")
 			}
 		}
+
+	case *parser.LoopStat:
+		v.nestedLoopCount[v.functions[len(v.functions)-1]]--
+	case *parser.FunctionDecl:
+		v.functions = v.functions[:len(v.functions)-1]
+		delete(v.nestedLoopCount, n.Function)
+	case *parser.LambdaExpr:
+		v.functions = v.functions[:len(v.functions)-1]
+		delete(v.nestedLoopCount, n.Function)
 	}
 }
 
